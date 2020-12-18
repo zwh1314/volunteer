8d61131016a0c9b4f83e83101920e030da22a951
Sherry <1803687047@qq.com>
2020-12-18 22:35:32 +0800

    杨怡歆更新 Activity,ActivityDao,ActivityServiceActivityServiceImpl,ActivityTest

---
+++ src/main/java/com/example/volunteer/Dao/ActivityDao.java
@@ -0,0 +1,32 @@
+package com.example.volunteer.Dao;
+
+import com.example.volunteer.DTO.ActivityDTO;
+import com.example.volunteer.Entity.Activity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+@Mapper
+public  interface ActivityDao {
+    @Insert("INSERT INTO activity(activity_name,activity_content,activity_organizer,activity_date) VALUES (#{activityName},#{activityContent},#{activityOrganizer},#{activityDate});")
+    int insertActivity(Activity activity);
+
+    @ResultType(ActivityDTO.class)
+    @Select("SELECT activity_id as activityId,activity_name as activityName,activity_content as activityContent,activity_organizer as activityOrganizer,activity_date as activityDate FROM activity WHERE activity_id = #{activityId}")
+    ActivityDTO getActivityByActivityId(@Param("activityId") long activityId);
+
+    @ResultType(ActivityDTO.class)
+    @Select("SELECT activity_id as activityId,activity_name as activityName,activity_content as activityContent,activity_organizer as activityOrganizer,activity_date as activityDate FROM activity WHERE activity_name = #{activityName}")
+    ActivityDTO getActivityByActivityName(@Param("activityName") String activityName);
+
+    @ResultType(ActivityDTO.class)
+    @Select("SELECT activity_id as activityId,activity_name as activityName,activity_content as activityContent,activity_organizer as activityOrganizer,activity_date as activityDate FROM activity WHERE activity_organizer = #{activityOrganizer}")
+    ActivityDTO getActivityByOrganizer(@Param("activityOrganizer") String activityOrganizer);
+
+    @Update("UPDATE activity SET activityName = #{activityName},activityOrganizer = #{activityOrganizer}, activityContent = #{activityContent},activityDate = #{activityDate} WHERE activityId = #{activityId}")
+    int updateActivity(Activity activity);
+
+    @Delete("Delete From activity WHERE activity_id=#{activityId}")
+    int deleteActivityByActivityId(@Param("activityId")long activityId);
+}
---
+++ src/test/java/com/example/volunteer/test/ActivityTest.java
@@ -0,0 +1,43 @@
+package com.example.volunteer.test;
+
+import com.example.volunteer.DTO.ActivityDTO;
+import com.example.volunteer.Dao.ActivityDao;
+import com.example.volunteer.Entity.Activity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivityTest {
+    @Autowired
+    private ActivityDao activityDao;
+
+    @Test
+    public void firstTest() throws ParseException {
+
+        Activity activity1=new Activity();
+        activity1.setActivityId(1);
+        activity1.setActivityName("活动1");
+        activity1.setActivityContent("喂猫");
+        activity1.setActivityOrganizer("二狗");
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = dateformat.parse("2020-12-25 00:00:00");
+        activity1.setActivityDate(date);
+        activityDao.insertActivity(activity1);
+        ActivityDTO activityDTO=activityDao.getActivityByActivityId(activity1.getActivityId());
+        System.out.println(activityDao.getActivityByActivityId(activity1.getActivityId()));
+
+
+    }
+    @Test
+    public void deleteTest(){
+        activityDao.deleteActivityByActivityId(1);
+    }
+}
