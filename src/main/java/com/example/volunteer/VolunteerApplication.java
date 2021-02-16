be233e83bc8058b9d841b03dd623296f5511b473
anti177 <anti177@users.noreply.github.com>
2021-02-16 22:47:23 +0800

    杨怡歆更新CreditsRedisService,RedisServiceImpl, ScheduledTasks, VolunteerApplication

--- src/main/java/com/example/volunteer/VolunteerApplication.java
+++ src/main/java/com/example/volunteer/VolunteerApplication.java
@@ -2,9 +2,11 @@ package com.example.volunteer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class VolunteerApplication {
 
 	public static void main(String[] args) {
